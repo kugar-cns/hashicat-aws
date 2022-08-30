9ffc1b59ae5cd56489d4c560fec4ce05264ea963
kugar-cns <kugar93@lgcns.com>
2022-08-30 08:41:49 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -2,12 +2,5 @@ module "s3_bucket" {
   source  = "app.terraform.io/kugar-training/s3-bucket/aws"
   version = "2.8.0"
 
-  bucket = "my-s3-bucket"
-  acl    = "private"
   bucket_prefix = "kugar"
-
-  versioning = {
-    enabled = true
-  }
-
 }
